f0039f79cbbaf29fab7379c6f1a54b437689ca21
Bill Brock <william.e.brock@gmail.com>
2017-01-27 09:26:41 -0500

    Stand Up the Project
    _Why?_
    
    So that people can get moving!
    
    _How?_
    
    Install storybook, jest and an simple stupid component.

---
+++ .storybook/config.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { configure, addDecorator } from '@kadira/storybook';
+
+const req = require.context('../components', true, /.stories.js$/)
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename))
+}
+
+const CenterDecorator = (story) => (
+  <div style={{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "0 0",
+    padding: "20",
+    height: "95vh"
+  }}>
+    {story()}
+  </div>
+)
+
+addDecorator(CenterDecorator)
+
+configure(loadStories, module);
---
+++ blueprints/stateless/files/__root__/components/__name__/component.js
@@ -0,0 +1,15 @@
+/* @flow */
+import React from 'react'
+import styles from './styles.css'
+
+function <%= pascalEntityName %> ({
+  title = '<%= pascalEntityName %>'
+}) {
+  return (
+    <div className={styles.container}>
+      {title}
+    </div>
+  )
+}
+
+export default <%= pascalEntityName %>
