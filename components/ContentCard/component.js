06dfe6b29e42f70d3dd5d79e0c44427c45e60d24
sonofabear <scott@spartansystems.co>
2017-02-03 14:58:04 -0500

    Added Component Logic
    Why? We wanted a card component
    
    How? Added card that contains an image, title, body, and a link. If link is empty, then the <a> tags are removed, same with the image field.

--- components/ContentCard/component.js
+++ components/ContentCard/component.js
@@ -2,18 +2,58 @@
 import React from 'react'
 import styles from './styles.css'
 
-function ContentCard ({
+type Props = {
+  title: string,
+  body: string,
+  image: string,
+  link: string
+};
+function renderImage (image, link) {
+  if (!image) {
+    return null
+  }
+  const imageHeaderStyle = {
+    backgroundImage: `url(${image})`
+  }
+  const imageMarkup = <div className={styles.image} style={imageHeaderStyle} />
+  if (link) {
+    return (
+      <a href={link}>
+        {imageMarkup}
+      </a>
+    )
+  }
+  return (imageMarkup)
+}
+
+function renderTitle (title, link) {
+  if (!title) {
+    return null
+  }
+  const titleMarkup = <h2 className={styles.title}>{title}</h2>
+  if (link) {
+    return (
+      <a href={link}>
+        {titleMarkup}
+      </a>
+    )
+  }
+  return (titleMarkup)
+}
+
+const ContentCard = ({
   title = 'ContentCard',
   body = 'testing some body text',
-  image = ''
-}) {
+  image = 'https://images.unsplash.com/photo-1485766410122-1b403edb53db?dpr=2&auto=compress',
+  link = 'http://joinspartan.com'
+}: Props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.image}>
-        <img src={image} />
+      {renderImage(image, link)}
+      <div className={styles.textWrapper}>
+        {renderTitle(title, link)}
+        <div className={styles.body}>{body}</div>
       </div>
-      <h2 className={styles.title}>{title}</h2>
-      <div className={styles.body}>{body}</div>
     </div>
   )
 }
--- components/ContentCard/stories.js
+++ components/ContentCard/stories.js
@@ -9,8 +9,9 @@ stories.addDecorator(withKnobs)
 
 stories.add('with text', () => (
   <ContentCard
+    link={text('Link URL', 'http://test.com')}
     image={text('Image URL', 'https://images.unsplash.com/uploads/141219324227007364f95/be0967a3?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=')}
     title={text('Header Title', 'Card Title')}
-    body={text('Card Body', 'Some Card Body Text')}
+    body={text('Card Body', 'Lomo echo park banh mi migas. Tote bag normcore scenester semiotics seitan, tofu try-hard shabby chic farm-to-table pitchfork humblebrag chicharrones. Aesthetic hexagon jianbing, stumptown tote bag everyday carry try-hard knausgaard gastropub activated charcoal vinyl distillery actually. Hexagon biodiesel kickstarter narwhal pug. Fixie bicycle rights occupy tofu, lyft cold-pressed umami organic knausgaard fashion axe irony godard post-ironic kickstarter flexitarian. Wayfarers sriracha occupy, four dollar toast art party hammock ramps. Messenger bag kombucha lyft echo park blog migas.')}
   />
-));
+))
