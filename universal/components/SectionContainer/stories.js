903f55782bfbbf64e7744313e4b411c249c64dbf
Andrew Frank <acfrank@me.com>
2017-02-10 13:09:57 -0500

    Story Book Notes (#35)
    Why
    ---
    
    - Designers and engineers need good documentation.
    
    This change addresses the need by
    ---------------------------------
    
    - Add in appropriate documentation
    - Notes added back into story file.

--- universal/components/SectionContainer/stories.js
+++ universal/components/SectionContainer/stories.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
-// import { WithNotes } from '@kadira/storybook-addon-notes'
+import { WithNotes } from '@kadira/storybook-addon-notes'
 import { withKnobs, text, select } from '@kadira/storybook-addon-knobs'
 
 import SectionContainer from './component'
 
-// const notes = 'This story demonstrates the props that can be passed to SectionContainer.'
+const notes = 'This story demonstrates the props that can be passed to SectionContainer.'
 
 const props = {
   customClass: 'customClass',
@@ -36,16 +36,16 @@ storiesOf('SectionContainer', module)
   .addDecorator(withKnobs)
   .add('with props', () => {
     return (
-      // <WithNotes notes={notes}>
-      <SectionContainer
-        customClass={text('type in a custom class that will show up in the DOM?', props.customClass)}
-        boxModel={select('What box model is this section?', props.boxModel, props.boxModel[0])}
-        bg={select('What background do you want? Default is transparent.', props.bg, props.bg[0])}
-      >
-        <div style={PlaceholderStyles}>
-          <p>Place this container inside of other elements and never worry about setting your grid container again!</p>
-        </div>
-      </SectionContainer>
-      // </WithNotes>
+      <WithNotes notes={notes}>
+        <SectionContainer
+          customClass={text('type in a custom class that will show up in the DOM?', props.customClass)}
+          boxModel={select('What box model is this section?', props.boxModel, props.boxModel[0])}
+          bg={select('What background do you want? Default is transparent.', props.bg, props.bg[0])}
+        >
+          <p style={PlaceholderStyles}>Lorem ipsum</p>
+          <p style={PlaceholderStyles}>Lorem ipsum</p>
+          <p style={PlaceholderStyles}>Lorem ipsum</p>
+        </SectionContainer>
+      </WithNotes>
     )
   })
